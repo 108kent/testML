cbd715a2753e587b3c49e7daa9523b0265afff74
Your Name <you@example.com>
2023-01-24 20:16:31 +0000

    test

--- dashboard/models.py
+++ dashboard/models.py
@@ -21,7 +21,8 @@ class Data(models.Model):
     date = models.DateTimeField(auto_now_add=True)
 
     def save(self, *args, **kwargs):
-        ml_model = pd.read_pickle('ml_model/model.pkl')
+        ml_model = pickle.load(open('ml_model/modelx.pkl', 'rb'))
+        #ml_model = joblib.load('ml_model/kento2.joblib')
         self.predictions = ml_model.predict(
             [[self.age, self.year_in_germany, self.sex]])
         return super().save(*args, *kwargs)
